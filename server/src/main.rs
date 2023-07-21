cf29119119b349231d228c4ece53f3fca78a9784
Mathew Fournier <mathew.fournier@gmail.com>
2023-07-20 20:20:30 -0700

    hello world server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-form
+//! ```
+
+use axum::{extract::Form, response::Html, routing::get, Router};
+use serde::Deserialize;
+use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_form=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with some routes
+    let app = Router::new().route("/", get(show_form).post(accept_form));
+
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
+
+async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!doctype html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/" method="post">
+                    <label for="name">
+                        Enter your name:
+                        <input type="text" name="name">
+                    </label>
+
+                    <label>
+                        Enter your email:
+                        <input type="text" name="email">
+                    </label>
+
+                    <input type="submit" value="Subscribe!">
+                </form>
+            </body>
+        </html>
+        "#,
+    )
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Input {
+    name: String,
+    email: String,
+}
+
+async fn accept_form(Form(input): Form<Input>) {
+    dbg!(&input);
+}
+
+//use axum::{
+//    extract,
+//    routing::post,
+//    Router,
+//};
+//use serde::Deserialize;
+
+//#[derive(Deserialize)]
+//struct CreateUser {
+//    email: String,
+//    password: String,
+//}
+//
+//async fn create_user(extract::Json(payload): extract::Json<CreateUser>) {
+//    // payload is a `CreateUser`
+//}
+
+//let app = Router::new().route("/users", post(create_user));

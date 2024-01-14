1f9700b9c9fc84590a7cc5e0ef430b3adaa023eb
Mathew Fournier <mathew.fournier@gmail.com>
2024-01-14 11:04:34 -0800

    postgres init

--- scripts/db.md
+++ scripts/db.md
@@ -6,3 +6,17 @@
  podman machine start
  podman pull docker.io/library/postgres:14
 ```
+
+Migrations managed by sqlx-cli 
+
+```
+cargo install sqlx-cli --no-default-features --features native-tls,postgres
+```
+
+need psql installed
+
+```
+brew install postgresql@16
+```
+
+Need to figure out where to get this on linux
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -17,6 +17,19 @@ podman run \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
   -e POSTGRES_DB=${DB_NAME} \
   -p "${DB_PORT}":5432 \
-  -d postgres:14 \
+  -d postgres:16 \
   postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
+  #
+
+# keep pinging postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} export DATABASE_URL
+sqlx database create
